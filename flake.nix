3339431432c00aad7b30c3d952ff44254ed956fa
sachesi <107267487+sachesi@users.noreply.github.com>
2026-05-29 08:36:17 +0000

    chore: release 0.3.5
    - translate the new dependency-operation strings (Ukrainian)
    - bump version to 0.3.5

--- flake.nix
+++ flake.nix
@@ -48,7 +48,7 @@
             rustc = rustToolchain;
           }).buildRustPackage {
             pname = "leyen";
-            version = "0.3.0";
+            version = "0.3.5";
             src = ./.;
 
             cargoLock = {
